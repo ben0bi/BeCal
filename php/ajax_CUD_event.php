c650be981b6ff9b2d4fd4992a804314db0f3e76e
benetzdiener <benetz@dien.er>
2019-10-03 05:54:23 +0200

    CUD to JSON, halfway done.

--- php/ajax_CUD_event.php
+++ php/ajax_CUD_event.php
@@ -1,9 +1,10 @@
 <?php
 
 // CUD - create, update or delete an item.
-// 
 
-require __DIR__."/sql.php";
+//require __DIR__."/sql.php";
+
+// new > 2.5.4: json saveing.
 
 $CUD=$_POST['CUD'];
 $dbid=$_POST['dbid'];
@@ -14,30 +15,78 @@ $summary=$_POST['summary'];
 $audiofile=$_POST['audiofile'];
 $color=$_POST['color'];
 $eventtype=$_POST['eventtype'];
+$userid=$_POST['userid'];
 
 echo("TO DB: $CUD $dbid $startdate $enddate $title $summary $color $audiofile");
 
-// This is done in the sql queries.
-//$title=SQL::textToSQL($title);
-//$summary=SQL::textToSQL($summary);
-//$audiofile=SQL::textToSQL($audiofile);
+$datafile = '../DATA/becaldatabase.gml';
 
-SQL::openConnection();
+// Read JSON file
+$json = file_get_contents($datafile);
+
+//Decode JSON
+$json_data = json_decode($json,true);
+
+echo("OS: $json ".sizeof($json_data['EVENTS']));
+
+if(sizeof($json_data['EVENTS'])<=0)
+	$json_data['EVENTS']=[];
+
+// SQL::openConnection();
+function get_Next_DBID()
+{
+	echo("GET NEXT DBID");
+	global $json_data;
+	$id=0;
+	$q=0;
+	foreach($json_data['EVENTS'] as $e)
+	{
+		$q++;
+		echo("ENTRY # ".$q);
+		$i=intval($e['ID']);
+		if($i>=$id)
+			$id=$i+1;
+	}
+	return $id;
+}
 
 // create or update an entry.
-if($CUD=='create')
+//if($CUD=='create')
 {
-	if($dbid==-1)
-	{
-		SQL::query(SQL::insert_event($title, $startdate, $enddate, $eventtype, $color, $audiofile, $summary));
-	}else{
+//	if($dbid==-1)
+//	{
+		//SQL::query(SQL::insert_event($title, $startdate, $enddate, $eventtype, $color, $audiofile, $summary));
+		if(sizeof(json_data['EVENTS'])<=0)
+		{
+			$json_data=[];
+			$json_data['EVENTS']=array();
+		}
+		$nen = [];
+		$nen['ID'] = get_Next_DBID();
+		$nen['TITLE']=$title;
+		$nen['STARTDATE']=$startdate;
+		$nen['ENDDATE']=$enddate;
+		$nen['EVENTTYPE']=$eventtype;
+		$nen['COLOR']=$color;
+		$nen['AUDIOFILE']=$audiofile;
+		$nen['SUMMARY']=$summary;
+		$nen['USERID']=$userid;
+		$json_data['EVENTS'][] = $nen;
+		$jdata = json_encode($json_data);
+		if(file_put_contents($datafile, $jdata))
+		{
+			echo("Added event: ".$nen['ID']." ".$title);
+	    	}else{
+	        	echo "Error while saving the data.";
+		}
+//	}else{
 		// get old audio file name and delete the file when the name does not match the new one.
-		$audiofilename = SQL::get_audio_filename($dbid);
+/*		$audiofilename = SQL::get_audio_filename($dbid);
 		if($audiofilename!=$summary && $audiofilename!="")
 			unlink("../DATA/AUDIO/$audiofilename");
 		SQL::query(SQL::update_event($dbid, $title, $startdate, $enddate, $eventtype, $color, $audiofile, $summary));
 	}
-	echo(" DB write done.");
+*/	echo(" DB write done.");
 }
 
 // delete an entry.
--- php/ajax_save_audiofile.php
+++ php/ajax_save_audiofile.php
@@ -4,20 +4,21 @@
 // audio files for BeCal
 
 //require __DIR__."/sql.php";
-if(isset($_FILES['file']) and !$_FILES['file']['error']){
-    
+if(isset($_FILES['file']) and !$_FILES['file']['error'])
+{
 	$fname=date('Y_m_d_H_i_s')."__".rand(1,10000).".WAV";
 	//$fname = "11" . ".wav";
 	$fdir="../DATA/AUDIO/$fname";
 
 	// maybe remove a file with the same name (unwanted because of unique ids)
-	if(file_exists($fdir)) {
+	if(file_exists($fdir))
+	{
 		chmod($fdir,0755); //Change the file permissions if allowed
 		unlink($fdir); //remove the file
 	}
-	
+
 	// copy the file to the new location.
-    move_uploaded_file($_FILES['file']['tmp_name'], $fdir);
+	move_uploaded_file($_FILES['file']['tmp_name'], $fdir);
 	// return the new filename.
 	echo($fname);
 }else{echo("ERROR: AUDIO NOT SAVED!");}
